e2e0b2f66fbdff06e1124a70ef552aa64e801bba
AsSerge <z00m.serge@gmail.com>
2021-10-20 17:56:12 +0300

    WorkCont

--- Modules/TaskList/get_task_number.php
+++ Modules/TaskList/get_task_number.php
@@ -9,6 +9,44 @@ include_once($_SERVER['DOCUMENT_ROOT']."/Login/classes/dbconnect.php"); //$pdo
 
 $customer_id = $_POST['customer_id'];
 // Получаем тип заказчика по 
+// Получаем тип заказчика по ID
+$stmt = $pdo->prepare("SELECT customer_type FROM customers WHERE customer_id = ?");
+$stmt->execute(array($customer_id));
+$customer_type = $stmt->fetch(PDO::FETCH_COLUMN); // Тип заказчика
 
+switch($customer_type){
+	case 'Розница':
+		$pref = 'РОЗ';
+		break;
+	case 'Сети':
+		$pref = 'СЕТ';
+		break;
+	case 'Опт':
+		$pref = 'ОПТ';
+		break;
+	case 'Маркетплейс':
+		$pref = 'МПЛ';
+		break;
+	case 'Логотип':
+		$pref = 'ЛОГ';
+		break;
+}
+
+$year = date("Y"); // Год добавления
+// Получаем номер последней добавленной задачи
+$stmt = $pdo->prepare("SELECT task_number FROM tasks WHERE customer_id = ? ORDER BY task_update");
+$stmt->execute(array($customer_id));
+$tasks = $stmt->fetchAll(PDO::FETCH_ASSOC); // Тип заказчика
+
+
+
+
+
+
+
+
+$num = 1;	// Номер добвления
+$num_str = sprintf("%08d", $num);// Заполнитель
+echo $pref."-".$year."-".$num_str;
 
 ?>
--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-// Утверждение креатива
 include_once($_SERVER['DOCUMENT_ROOT']."/Login/classes/dbconnect.php"); //$pdo
 // 'Розница'	    РОЗ-2021-00001
 // 'Сети'          СЕТ
@@ -8,32 +7,45 @@ include_once($_SERVER['DOCUMENT_ROOT']."/Login/classes/dbconnect.php"); //$pdo
 // 'Логотип'       ЛОГ
 
 // $customer_id = $_POST['customer_id'];
-$customer_id = "3";
+$customer_id = 3;
 // Получаем тип заказчика по ID
 $stmt = $pdo->prepare("SELECT customer_type FROM customers WHERE customer_id = ?");
 $stmt->execute(array($customer_id));
 $customer_type = $stmt->fetch(PDO::FETCH_COLUMN); // Тип заказчика
 
+switch($customer_type){
+	case 'Розница':
+		$pref = 'РОЗ';
+		break;
+	case 'Сети':
+		$pref = 'СЕТ';
+		break;
+	case 'Опт':
+		$pref = 'ОПТ';
+		break;
+	case 'Маркетплейс':
+		$pref = 'МПЛ';
+		break;
+	case 'Логотип':
+		$pref = 'ЛОГ';
+		break;
+}
+
+$year = date("Y"); // Год добавления
 // Получаем номер последней добавленной задачи
-$stmt = $pdo->prepare("SELECT task_number FROM tasks WHERE customer_id = ?");
-$stmt->execute(array($customer_id));
-$tasks = $stmt->fetchAll(PDO::FETCH_ASSOC); // Тип заказчика
+// $stmt = $pdo->prepare("SELECT task_number FROM tasks AS T LEFT JOIN customers AS C ON (T.customer_id = C.customer_id) WHERE customer_type = ? ORDER BY T.task_update DESC");
+$stmt = $pdo->prepare("SELECT MAX(task_number) FROM tasks AS T LEFT JOIN customers AS C ON (T.customer_id = C.customer_id) WHERE customer_type = ?");
+$stmt->execute(array($customer_type));
+$tasks = $stmt->fetch(PDO::FETCH_COLUMN); // Тип заказчика ВРОДЕ ПОСЛЕДНЯЯ ЗАПИСЬ т.к. фильтр по убыванию
 
 
+echo $tasks;
 
-// echo $customer_type;
-// echo "<pre>";
-// print_r($tasks);
-// echo "</pre>";
+echo "<br>";
 
 
-// $stmt = $pdo->prepare("SELECT * FROM tasks AS T LEFT JOIN customers AS C ON (T.customer_id = C.customer_id) WHERE C.customer_id = ?");
-// $stmt->execute(array($customer_id));
-// $task_number = $stmt->fetchALL(PDO::FETCH_ASSOC);
-
-// echo "<pre>";
-// print_r($task_number);
-// echo "</pre>";
-
+// $num = 1;	// Номер добвления
+// $num_str = sprintf("%08d", $num);// Заполнитель
+// echo $pref."-".$year."-".$num_str;
 
 ?>
